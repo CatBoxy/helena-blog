454852d0b1e258c731f13452d06fd65cd6f0a5f1
Juan <thecatbox@protonmail.com>
2022-07-12 10:15:28 -0300

    button fix

--- src/components/BookBody/styles.ts
+++ src/components/BookBody/styles.ts
@@ -15,17 +15,17 @@ export const Container = styled.div`
         padding: 3px;
     }
 
-    div > button {
+    button {
         color: ${(props) => (props.theme.colors.primary)};
         padding: 6px 15px;
         border-radius: 3px;
     }
 
-    div > button:hover {
+    button:hover {
         background-color: #F9D4C7;
     }
 
-    div > button:active {
+    button:active {
         background-color: #F19D7E;
     }
 `;
